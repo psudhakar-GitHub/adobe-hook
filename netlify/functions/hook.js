331663b7fbeb52269f4fe210e8b1f4902af2cc03
psudhakar-GitHub <42378022+psudhakar-GitHub@users.noreply.github.com>
2023-09-21 10:10:12 +0530

    removed all validations for testing

--- netlify/functions/hook.js
+++ netlify/functions/hook.js
@@ -1,27 +1,13 @@
 import fetch from "node-fetch";
 
 export const handler = async (event, context) => {
-  var body = JSON.parse(event.body);
   console.log(event);
   
-  // Fetch client id
-  var clientid = body["X-AdobeSign-ClientId"];
-  console.log(clientid);
-  
-  // if (clientid === "UB7E5BXCXY") {
-    return {
+  return {
       statusCode: 200,
-      // body: JSON.stringify({
-      //   message: "SUCCESS"
-      // }),
-      headers: { "X-AdobeSign-ClientId": clientid }
-    }
-  // }
-
-  // return {
-  //   statusCode: 500,
-  //   body: JSON.stringify({
-  //     message: "FAILED!",
-  //   }),
-  // };
+      body: JSON.stringify({
+        message: "SUCCESS",
+      }),
+      headers: { "X-AdobeSign-ClientId": 'UB7E5BXCXY' },
+    };
 };
