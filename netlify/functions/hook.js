6d273badc38cbad3d80d4ea9867c460c60cdad73
psudhakar-GitHub <42378022+psudhakar-GitHub@users.noreply.github.com>
2023-09-21 10:26:20 +0530

    final version for hook updated

--- netlify/functions/hook.js
+++ netlify/functions/hook.js
@@ -1,13 +1,31 @@
-import fetch from "node-fetch";
-
 export const handler = async (event, context) => {
-  console.log(event);
-  
-  return {
-      statusCode: 200,
+   var eventParsed = JSON.parse(event);
+
+  // webhook registration
+  if (eventParsed.httpMethod === "GET") {
+    const userAgent = eventParsed.multiValueHeaders["User-Agent"];
+    const clientId = eventParsed.multiValueHeaders["X-Adobesign-Clientid"];
+
+    if (userAgent === "AdobeSign" && clientId === "UB7E5BXCXY") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "SUCCESS",
+        }),
+        headers: { "X-AdobeSign-ClientId": clientId },
+      };
+    }
+
+    return {
+      statusCode: 500,
       body: JSON.stringify({
-        message: "SUCCESS",
+        message: "FAILED!",
       }),
-      headers: { "X-AdobeSign-ClientId": 'UB7E5BXCXY' },
     };
+  }
+
+  // capture adobe events here
+  if (eventParsed.httpMethod === "POST") {
+    console.log(eventParsed.body);
+  }
 };
